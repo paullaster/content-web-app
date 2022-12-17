5bd54210180b437529585761b6c7054bd1aaee02
paullaster <paullasterokoth98@gmail.com>
2022-12-18 00:21:55 +0300

    app setting

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import express from 'express';
 //Creating app instance
 const app = express();
 
+//Application settings
+app.use ( express.json ());
+app.use ( express.urlencoded ( {extended: true} ));
+
 //Instanciating server
 app.listen (6000, () => {
     console.log (`App listening on port:6000`);
