17476e4c73094bbecfe139ded965cfd23c1699a1
paullaster <paullasterokoth98@gmail.com>
2022-12-18 00:27:16 +0300

    serve

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //Importing dependencies
 import express from 'express';
 
+//Internal dependencies
+
 //Creating app instance
 const app = express();
 
 //Application settings
 app.use ( express.json ());
 app.use ( express.urlencoded ( {extended: true} ));
+
+//middleware
 
 //Instanciating server
 app.listen (6000, () => {
