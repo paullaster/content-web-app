cfee10b454c1574d312853fc7dce079638196e04
paullaster <paullasterokoth98@gmail.com>
2022-12-18 11:31:30 +0300

    blogg class

---
+++ controllers/services/blogs/index.js
@@ -0,0 +1,9 @@
+//Blog Index file
+class Blog {
+    constructor (title, content, author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    };
+    
+}
