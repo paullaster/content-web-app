6ef943c7e3445a9b47df6caff9d447faee97df8a
paullaster <paullasterokoth98@gmail.com>
2022-12-19 00:15:24 +0300

    fetch blogs

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,15 +1,22 @@
 //dependencies
-import JWT from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import JWT from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 //Internal dependencies
-import User from '../../models/user.schema';
+import User from "../../models/user.schema";
 
-const login = ( req, res) => {
-    const { password, ...arg } = req.body;
-    for ( let prop in arg) {
-
-    }
-    
+const login = (req, res) => {
+  const { password, email } = req.body;
+    User.findOne({ email: email })
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        res
+        .status (404)
+        .json ( {
+            code: 'error'
+        })
+      });
 };
 export default login;
