b427ca830c130f173948ffab3f9d3a0b8616046f
paullaster <paullasterokoth98@gmail.com>
2022-12-18 17:01:28 +0300

    fetch blogs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,1 +1,9 @@
 //Dependencies
+import express from 'express';
+
+//Internal dependencies
+import Accounts from '../controllers/auth/index';
+
+const Auth = express.Router();
+
+Auth.route('/register').post ( Accounts.new)
