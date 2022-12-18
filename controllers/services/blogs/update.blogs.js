87e880b04476dc00ab79dea55763b1531bb30c98
paullaster <paullasterokoth98@gmail.com>
2022-12-18 15:25:59 +0300

    fetch blogs

--- controllers/services/blogs/update.blogs.js
+++ controllers/services/blogs/update.blogs.js
@@ -1,12 +1,27 @@
 import BlogModel from "../../../models/blog.schema";
 const updateBlogs = (req, res) => {
-    const {title, body, blogId} = req.body;
+    const {blogId} = req.query;
+    const {title, body} = req.body;
     BlogModel.find({
         _id: blogId,
     })
     .then ( (blog) => {
-        res
-        .json(blog)
+        if (blog.length !== 1) {
+            res
+            .status(404)
+            .json ( {
+                code: 'error',
+                error: 'Blog not found!'
+            })
+        };
     })
+    .catch ( (err) => {
+        res
+        .status(500)
+        .json ( {
+            code: 'error',
+            error: err.message,
+        });
+    });
 };
 export default updateBlogs;
