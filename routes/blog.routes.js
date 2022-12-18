2e86ff603bdc5f49f8b73c87c291962fd1689517
paullaster <paullasterokoth98@gmail.com>
2022-12-18 12:24:39 +0300

    blog routes set up

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -7,3 +7,5 @@ import createBlog from '../controllers/services/blogs/create.blog';
 import fetchBlogs from '../controllers/services/blogs/fetch.blog';
 import updateBlogs from '../controllers/services/blogs/update.blogs';
 import deleteBlogs from '../controllers/services/blogs/delete.blogs';
+
+route('/new').post (createBlog);
