7534d275bb07e9c8fe8ec84279c73bd8d6930b79
paullaster <paullasterokoth98@gmail.com>
2022-12-18 23:41:33 +0300

    fetch blogs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,5 +7,6 @@ import Accounts from '../controllers/auth/index';
 const Auth = express.Router();
 
 Auth.route('/register').post ( Accounts.new);
+Auth.route('/login').post (Accounts.login);
 
 export default Auth;
