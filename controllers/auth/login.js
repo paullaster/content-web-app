008f9372f5d25e8af1b657b364c3af9e99900ecc
paullaster <paullasterokoth98@gmail.com>
2022-12-19 00:06:37 +0300

    fetch blogs

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -6,22 +6,10 @@ import bcrypt from 'bcrypt';
 import User from '../../models/user.schema';
 
 const login = ( req, res) => {
-    const { password, input } = req.body;
-    User.findOne({ email: input})
-    .then(() =>{})
-    .catch( (err) => {
-        User.findOne( {phonenumber: input})
-        .then ( (user) => {})
-        .catch( (err) => {
-            res
-            .status(404)
-            .json (
-                {
-                    code: 'error',
-                    error: "phonenumber or email not found"
-                }
-            )
-        })
-    });
+    const { password, ...arg } = req.body;
+    for ( let prop in arg) {
+
+    }
+    
 };
 export default login;
