fa2ebc9e4d8cf58c8bffeca1635affdef1f99914
paullaster <paullasterokoth98@gmail.com>
2022-12-19 00:52:14 +0300

    fetch blogs

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -37,7 +37,7 @@ const login = (req, res) => {
                     });
                     return;
                 };
-                
+                res.json(token);
             })
         })
         .catch((err) => {
