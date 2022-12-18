6cfeb3b3ea691aad94cd68bb0c735eaa58cd5617
paullaster <paullasterokoth98@gmail.com>
2022-12-18 12:23:03 +0300

    blog mehods set up

---
+++ routes/blog.routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const route = express.Router();
+
+
+//Internal dependencies
+import createBlog from '../controllers/services/blogs/create.blog';
+import fetchBlogs from '../controllers/services/blogs/fetch.blog';
+import updateBlogs from '../controllers/services/blogs/update.blogs';
+import deleteBlogs from '../controllers/services/blogs/delete.blogs';
