7695e20a6ed2bda70b7f907977b3fb8dae587825
paullaster <paullasterokoth98@gmail.com>
2022-12-18 16:40:36 +0300

    fetch blogs

--- controllers/services/blogs/index.js
+++ controllers/services/blogs/index.js
@@ -1,6 +1,14 @@
 //Blog Index file
-import blog from '../../../models/blog.schema';
+import createBlog from "./create.blog";
+import updateBlogs from "./update.blogs";
+import deleteBlogs from "./delete.blogs";
+import fetchBlogs from "./fetch.blog";
 
 //Blog
-
+const Blog = {
+    new: createBlog,
+    update: updateBlogs,
+    delete: deleteBlogs,
+    fetch: fetchBlogs
+}
 export default Blog;
