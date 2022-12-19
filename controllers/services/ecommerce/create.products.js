d2195cad4fab3a137d6a5d66ecfe359efe483736
paullaster <paullasterokoth98@gmail.com>
2022-12-19 17:47:42 +0300

    .

--- controllers/services/ecommerce/create.products.js
+++ controllers/services/ecommerce/create.products.js
@@ -1,11 +1,16 @@
 //CREATING PRODUCT
 import Product from '../../../models/product.schema';
-
+import Attribute from '../../../models/products.attribute.model';
 const createProduct = (req, res) => {
-    const {...product} = req.body;
+    const { variation, ...product} = req.body;
     let newProduct = new Product ( {
         ...product,
+    });
+    for (let item of variation) {};
+    let newAttribute = new Attribute ( {
+        ...variation,
     })
-    res.json (newProduct);
+    console.log([...variation])
+    res.json ({newProduct, newAttribute});
 };
 export default createProduct;
