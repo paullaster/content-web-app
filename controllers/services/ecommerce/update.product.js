0b61ae35fbc6fa28a5f48a694c37eb30099869f4
paullaster <paullasterokoth98@gmail.com>
2022-12-20 02:30:13 +0300

    .

--- controllers/services/ecommerce/update.product.js
+++ controllers/services/ecommerce/update.product.js
@@ -2,13 +2,18 @@
 import Product from '../../../models/product.schema';
 const updateProduct = (req, res) =>{
     const {_id} = req.params;
-    const {title, description, image, price, variation} = req.body;
+    const {title, description, image, price, variation:{color, size,quantity}} = req.body;
     Product.findOne({_id:_id})
     .then ((product) =>{
         Product.updateOne(
             {_id:product._id},
-
+            [
+             { $set: {title:title, description:description, image:image, price:price}}
+            ]
             )
+        .then ( () => {
+            
+        })
     })
     .catch((err) =>{
         res
