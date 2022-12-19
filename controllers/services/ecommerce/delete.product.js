683fa675d6e81105c1198902589fac930c304d87
paullaster <paullasterokoth98@gmail.com>
2022-12-20 02:20:18 +0300

    .

--- controllers/services/ecommerce/delete.product.js
+++ controllers/services/ecommerce/delete.product.js
@@ -1,6 +1,26 @@
 //FETCH ALL PRODUCTS
 import Product from '../../../models/product.schema';
 const deleteProduct = (req, res) =>{
-
+    const {_id} = req.params;
+    Product.findById({_id: _id})
+    .then((product) =>{
+        Product.deleteOne( {_id:product._id})
+        .then(() =>{
+            res
+            .status(200)
+            .json({
+                code: 'success',
+                message: 'Product deleted successfully',
+            })
+        })
+    })
+    .catch((error) =>{
+        res
+        .status(500)
+        .json( {
+            code: 'error',
+            error: error.message,
+        });
+    });
 };
 export default  deleteProduct;
--- controllers/services/ecommerce/index.js
+++ controllers/services/ecommerce/index.js
@@ -2,10 +2,12 @@
 import createProduct from  './create.products';
 import fetchProducts from './fetch.products';
 import fetchSingleProduct from './fetch.single.product';
+import deleteProduct from './delete.product';
 
 const Product = {
     new: createProduct,
     all_products: fetchProducts,
-    single_product: fetchSingleProduct
+    single_product: fetchSingleProduct,
+    delete: deleteProduct,
 };
 export default Product;
