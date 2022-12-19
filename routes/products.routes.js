cbe6f9b34e7f91f3d091a98a703c7efabc5fc96d
paullaster <paullasterokoth98@gmail.com>
2022-12-19 17:02:14 +0300

    .

---
+++ routes/products.routes.js
@@ -0,0 +1,9 @@
+//Dependencies
+import express from 'express';
+
+//Internal dependencies
+import Product from '../controllers/services/ecommerce/index';
+
+const ProductRoutes = express.Router();
+
+
