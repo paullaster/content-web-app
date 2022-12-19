e48d77ea0b99e08cb1684f7f1bc929c69481f7d7
paullaster <paullasterokoth98@gmail.com>
2022-12-20 02:21:15 +0300

    .

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -9,5 +9,5 @@ const ProductRoutes = express.Router();
 ProductRoutes.route( '/new').post (Product.new);
 ProductRoutes.route( '/all_products').get (Product.all_products);
 ProductRoutes.route( '/one_products/:_id').get (Product.single_product);
-
+ProductRoutes.route( '/delete/:_id').delete (Product.delete);
 export default ProductRoutes;
