5cd1f9d5cadd2b08ef03f6e7762e41de4f7cfc55
paullaster <paullasterokoth98@gmail.com>
2022-12-19 17:19:05 +0300

    .

--- models/products.attribute.model.js
+++ models/products.attribute.model.js
@@ -3,3 +3,5 @@ import mongoose from 'mongoose';
 import Attribute from './product.attribute';
 
 const AttributeModel = mongoose.model ('Product attribute', Attribute);
+
+export default AttributeModel
