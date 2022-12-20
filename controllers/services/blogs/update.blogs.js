8dcf86430979895f8dc007156ab02694c110774a
paullaster <paullasterokoth98@gmail.com>
2022-12-21 02:09:39 +0300

    add

--- controllers/services/blogs/update.blogs.js
+++ controllers/services/blogs/update.blogs.js
@@ -1,10 +1,29 @@
-
+import db from "../../../utils/database.connection";
 const updateBlogs = (req, res) => {
     const {blogid} = req.params;
     const {title, body} = req.body;
     
     let query = `UPDATE blog SET title = '${title}', 
-    content = '${body}' WHERE blogid = '${blogid}'`
+    content = '${body}' WHERE blogid = '${blogid}'`;
+    db.query(query, (err, res) => {
+        if (err) {
+            res
+            .status (500)
+            .json ( {
+                status: 'error',
+                error: err.message,
+            });
+            return;
+        };
+        res
+        .status (200)
+        .json (
+            {
+                status: 'success',
+                message: 'Blog with blog id ' + blogid + ' updated successfully',
+            }
+        );
+    });
 };
 export default updateBlogs;
 
