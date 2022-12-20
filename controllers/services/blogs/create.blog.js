f12f78eab8c98d5a50de2530d01020797d67c30f
paullaster <paullasterokoth98@gmail.com>
2022-12-20 23:49:39 +0300

    add

--- controllers/services/blogs/create.blog.js
+++ controllers/services/blogs/create.blog.js
@@ -2,11 +2,13 @@ import mysql from "mysql2";
 import db from '../../../utils/database.connection';
 const createBlog = (req, res) => {
     const {title, body} = req.body;
+
     let newBlog = new BlogModel({
         Title: title,
-        Content: body
+        Content: body,
+        date: Date.now()
     });
-    let query = 'INSERT INTO blog';
+    let query = 'INSERT INTO blog SET?';
     db.query()
 };
 export default createBlog;
