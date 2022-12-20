59d140c614950b7c1f0c6c98d478c88cd0a7ffc4
paullaster <paullasterokoth98@gmail.com>
2022-12-21 02:02:08 +0300

    add

--- controllers/services/blogs/fetch.blog.js
+++ controllers/services/blogs/fetch.blog.js
@@ -1,8 +1,25 @@
 import db from "../../../utils/database.connection";
 const fetchBlogs = (req, res) => {
     let query = 'SELECT * FROM blog';
-    db.query(query, (err, result) => {
-        
+    db.query(query, (err, rows) => {
+        if (err) {
+            res
+            .status (500)
+            .json ( {
+                status: 'error',
+                error: err.message,
+            });
+            return;
+        };
+        res
+        .status (200)
+        .json (
+            {
+                status: 'success',
+                message: 'Blogs retrieved successfully ',
+                data: rows,
+            }
+        );
     }); 
 };
 export default fetchBlogs;
