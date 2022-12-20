b448acad73034dccf9835d36bb6290cc6e49fc74
paullaster <paullasterokoth98@gmail.com>
2022-12-20 23:17:20 +0300

    add

--- models/products.attribute.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-import Attribute from './product.attribute';
-
-const AttributeModel = mongoose.model ('Product attribute', Attribute);
-
-export default AttributeModel
