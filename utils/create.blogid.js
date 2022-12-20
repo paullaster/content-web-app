00e39f631b20a4383d1fd0f2cef53d74e8551849
paullaster <paullasterokoth98@gmail.com>
2022-12-20 23:55:12 +0300

    add

--- utils/create.blogid.js
+++ utils/create.blogid.js
@@ -1,2 +1,6 @@
-const blogId = () => {};
+const blogId = () => {
+    const statement = 'blog';
+    let randNumber = [];
+    for ( let i = 0; randNumber.length < 4; i++ ) {};
+};
 export default blogId;
