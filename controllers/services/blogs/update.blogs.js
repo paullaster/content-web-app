dc034b31ede3381804d0957c0fdc345cd37a6848
paullaster <paullasterokoth98@gmail.com>
2022-12-21 02:04:23 +0300

    add

--- controllers/services/blogs/update.blogs.js
+++ controllers/services/blogs/update.blogs.js
@@ -1,28 +1,9 @@
-import BlogModel from "../../../models/blog.schema";
+
 const updateBlogs = (req, res) => {
-    const {blogId} = req.params;
+    const {blogid} = req.params;
     const {title, body} = req.body;
-    BlogModel.updateOne({
-        _id: blogId,
-    },[
-        {$set: {Title: title, Content: body}}
-    ])
-    .then ( (blog) => {
-        res
-        .status(200)
-        .json({
-            code: 'success',
-            message: 'Updated blog successfully',
-        });
-    })
-    .catch ( (err) => {
-        res
-        .status(500)
-        .json ( {
-            code: 'error',
-            error: err.message,
-        });
-    });
+    
+    let query = `UPDATE blog SET `
 };
 export default updateBlogs;
 
