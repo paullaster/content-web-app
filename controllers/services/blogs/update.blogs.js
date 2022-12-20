cc2acb4f516b4f4a1fb49a935dd817405aa25779
paullaster <paullasterokoth98@gmail.com>
2022-12-21 02:06:42 +0300

    add

--- controllers/services/blogs/update.blogs.js
+++ controllers/services/blogs/update.blogs.js
@@ -3,7 +3,8 @@ const updateBlogs = (req, res) => {
     const {blogid} = req.params;
     const {title, body} = req.body;
     
-    let query = `UPDATE blog SET title = '${title}', content = '${body}`
+    let query = `UPDATE blog SET title = '${title}', 
+    content = '${body}' WHERE blogid = '${blogid}'`
 };
 export default updateBlogs;
 
