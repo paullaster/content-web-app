8d2f20bbb01e7b61a5e28f13a6c5693a5eac2bd6
paullaster <paullasterokoth98@gmail.com>
2022-12-21 21:49:23 +0300

    add files

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -7,7 +7,7 @@ import Blog from '../controllers/services/blogs/index';
 
 BlogRoutes.route('/new').post (Blog.new);
 BlogRoutes.route('/get').get(Blog.fetch);
-BlogRoutes.route('/update').put (Blog.update);
+BlogRoutes.route('/update/:blogid').put (Blog.update);
 BlogRoutes.route('/delete/:blogid').delete (Blog.delete);
 
 export default BlogRoutes;
