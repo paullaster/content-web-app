d6b03a0dc6cfe4d8c66d84bccadeed66d3c4b877
paullaster <paullasterokoth98@gmail.com>
2022-12-21 19:15:08 +0300

    add files

--- controllers/services/ecommerce/delete.product.js
+++ controllers/services/ecommerce/delete.product.js
@@ -1,27 +1,21 @@
 //FETCH ALL PRODUCTS
 import db from "../../../utils/database.connection";
-const deleteProduct = (req, res) =>{
-    const {productid} = req.params;
-    
-    let query = `DELETE FROM products WHERE productid = '${productid}'`;
-    db.query (query, (err, rows) => {
-        if (err) {
-            res
-            .status (500)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
-            return;
-        };
-        res
-        .status (200)
-        .json (
-            {
-                status: 'success',
-                message: 'Blog with id ' + blogid +' deleted'
-            }
-        );
+const deleteProduct = (req, res) => {
+  const { productid } = req.params;
+
+  let query = `DELETE FROM products WHERE productid = '${productid}'`;
+  db.query(query, (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        status: "error",
+        error: err.message
+      });
+      return;
+    }
+    res.status(200).json({
+      status: "success",
+      message: "Blog with id " + blogid + " deleted"
     });
+  });
 };
-export default  deleteProduct;
+export default deleteProduct;
