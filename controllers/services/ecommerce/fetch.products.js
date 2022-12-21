2246b943b7d9bc00c5031ff2d74921b7b5efc45b
paullaster <paullasterokoth98@gmail.com>
2022-12-21 21:22:20 +0300

    add files

--- controllers/services/ecommerce/fetch.products.js
+++ controllers/services/ecommerce/fetch.products.js
@@ -5,6 +5,7 @@ import db from "../../../utils/database.connection";
 const fetchProducts = (req, res) =>{
     let query = `SELECT products.productid AS id , products.title AS title, 
     products.description AS description, products.price AS price, products.image AS image,
-     `;
+    attributes.variationid AS variation_id, attributes.color AS color, 
+    attributes.size AS size, attributes.quantity AS quantity`;
 };
 export default  fetchProducts;
