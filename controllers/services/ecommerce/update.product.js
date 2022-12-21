056f142ae7f87a1bb822aaa544aecccabfe91a8c
paullaster <paullasterokoth98@gmail.com>
2022-12-21 21:04:02 +0300

    add files

--- controllers/services/ecommerce/update.product.js
+++ controllers/services/ecommerce/update.product.js
@@ -26,35 +26,19 @@ const updateProduct = (req, res) => {
     let query = `UPDATE attributes SET color = '${color}', 
     size = '${size}', quantity = '${quantity}'
     WHERE productid = '${productid}'`;
-    db.query(query, (err, result) => {});
-  });
-  Product.findOne({ _id: _id })
-    .then(product => {
-      Product.updateOne({ _id: product._id }, [
-        {
-          $set: {
-            title: title,
-            description: description,
-            image: image,
-            price: price
-          }
-        }
-      ]).then(() => {
-        AttributeModel.updateOne({ _id: product._id }, [
-          { $set: { color: color, size: size, quantity: quantity } }
-        ]).then(() => {
-          res.status(200).json({
-            code: "success",
-            message: "Product updated successfully!"
-          });
+    db.query(query, (err, rows) => {
+        if (err) {
+            res.status(404).json({
+                status: "error",
+                error: err.message,
+            });
+            return;
+        };
+        res.status(200).json({
+            status: 'success',
+            message: "Product updated successfully",
         });
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        code: "error",
-        error: err.message
-      });
     });
+  });
 };
 export default updateProduct;
