05e4e38befc6ec061215be7794730abbd7e4f0af
paullaster <paullasterokoth98@gmail.com>
2022-12-21 22:06:36 +0300

    add files

--- controllers/services/blogs/update.blogs.js
+++ controllers/services/blogs/update.blogs.js
@@ -3,8 +3,8 @@ const updateBlogs = (req, res) => {
     const {blogid} = req.params;
     const {title, body} = req.body;
     
-    let query = `UPDATE blog SET title='${title}', 
-    content = '${body}' WHERE blogid='${blogid}'`;
+    let query = `UPDATE blog SET title=${title}, 
+    content =${body} WHERE blogid=${blogid}`;
     db.query(query, (err, rows) => {
         if (err) {
             res.status(500).json ( {
