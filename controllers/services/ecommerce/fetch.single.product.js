49c984920d2dc7394391207dc72ac47f6c92d904
paullaster <paullasterokoth98@gmail.com>
2022-12-21 21:30:49 +0300

    add files

--- controllers/services/ecommerce/fetch.single.product.js
+++ controllers/services/ecommerce/fetch.single.product.js
@@ -3,6 +3,14 @@ import db from "../../../utils/database.connection";
 const fetchSingleProduct = (req, res) =>{
     const {productid} = req.params;
     
-    
+    let query = `
+    SELECT products.productid AS id , products.title AS title, 
+    products.description AS description, products.price AS price, products.image AS image,
+    attributes.variationid AS variation_id, attributes.color AS color, 
+    attributes.size AS size, attributes.quantity AS quantity WHERE productid = '${productid}'
+    `;
+    db.query(query, (err, rows) =>{
+        
+    });
 };
 export default  fetchSingleProduct;
