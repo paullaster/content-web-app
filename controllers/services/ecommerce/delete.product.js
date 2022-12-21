ba7b149b17ca7380b58691db9d9926ff15728ff4
paullaster <paullasterokoth98@gmail.com>
2022-12-21 19:13:26 +0300

    add files

--- controllers/services/ecommerce/delete.product.js
+++ controllers/services/ecommerce/delete.product.js
@@ -2,25 +2,6 @@
 import db from "../../../utils/database.connection";
 const deleteProduct = (req, res) =>{
     const {productid} = req.params;
-    Product.findById({_id: _id})
-    .then((product) =>{
-        Product.deleteOne( {_id:product._id})
-        .then(() =>{
-            res
-            .status(200)
-            .json({
-                code: 'success',
-                message: 'Product deleted successfully',
-            })
-        })
-    })
-    .catch((error) =>{
-        res
-        .status(500)
-        .json( {
-            code: 'error',
-            error: error.message,
-        });
-    });
+    
 };
 export default  deleteProduct;
