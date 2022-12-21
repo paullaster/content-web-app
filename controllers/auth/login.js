1f57fe9e9f4f8f8e7741aa6366832b75bc5b6bd8
paullaster <paullasterokoth98@gmail.com>
2022-12-21 17:51:06 +0300

    add files

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -10,6 +10,69 @@ import db from "../../utils/database.connection";
 const login = (req, res) => {
   const { password, email } = req.body;
 
-    const query = `SELECT `
+    const query = `SELECT email, password FROM users WHERE email='${email}'`;
+    db.query (query, (err, rows) => {
+        if (err) {
+            res
+            .status (500)
+            .json ( {
+                status: 'error',
+                error: err.message,
+            })
+            return;
+        };
+
+        bcrypt.compare (password, rows[0].password)
+        .then((resp) =>{
+            if (!resp) {
+                res
+                .status(404)
+                .json (
+                    {
+                        code: 'success',
+                        error: 'Invalid password',
+                    }
+                );
+                return;
+            };
+            let payload = {email: rows[0].email}
+            JWT.sign (payload, process.env.TOKEN_SECRET, {
+                algorithm: 'HS512',
+                expiresIn: 5400,
+            }, (err, token) => {
+                if (err) {
+                    res
+                    .status(500)
+                    .json ( {
+                        code: 'error',
+                        error: 'Error creating token',
+                    });
+                    return;
+                };
+               
+                res
+                .status(200)
+                .header ( {
+                    'Authorization': 'Bearer ' +token
+                })
+                .json (
+                    {
+                        code: 'success',
+                        token: token,
+                    }
+                );
+            });
+        })
+        .catch((err) => {
+            res
+            .status(500)
+            .json(
+                {
+                    code: 'error',
+                    error: err.message,
+                }
+            );
+        });
+    });
 };
 export default login;
