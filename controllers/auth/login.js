3c27ea9cb02d6878733a8e7d845ac85665ab1084
paullaster <paullasterokoth98@gmail.com>
2022-12-21 17:24:45 +0300

    add files

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -3,11 +3,13 @@ import JWT from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import * as dotenv from "dotenv";
 dotenv.config ();
+
 //Internal dependencies
-import User from "../../models/user.schema";
+import db from "../../utils/database.connection";
 
 const login = (req, res) => {
   const { password, email } = req.body;
+  
     User.findOne({ email: email })
       .then(user => {
         bcrypt.compare (password, user.password)
