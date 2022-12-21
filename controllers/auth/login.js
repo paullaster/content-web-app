3a9ae3ac80bf5fa7b83b1ccfc035b19fd5090ace
paullaster <paullasterokoth98@gmail.com>
2022-12-21 17:27:02 +0300

    add files

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -9,68 +9,7 @@ import db from "../../utils/database.connection";
 
 const login = (req, res) => {
   const { password, email } = req.body;
-  
-    User.findOne({ email: email })
-      .then(user => {
-        bcrypt.compare (password, user.password)
-        .then((resp) =>{
-            if (!resp) {
-                res
-                .status(404)
-                .json (
-                    {
-                        code: 'success',
-                        error: 'Invalid password',
-                    }
-                );
-                return;
-            };
-            let payload = {_id: user._id}
-            JWT.sign (payload, process.env.TOKEN_SECRET, {
-                algorithm: 'HS512',
-                expiresIn: 5400,
-            }, (err, token) => {
-                if (err) {
-                    res
-                    .status(500)
-                    .json ( {
-                        code: 'error',
-                        error: 'Error creating token',
-                    });
-                    return;
-                };
-               
-                res
-                .status(200)
-                .header ( {
-                    'Authorization': 'Bearer ' +token
-                })
-                .json (
-                    {
-                        code: 'success',
-                        token: token,
-                    }
-                );
-            });
-        })
-        .catch((err) => {
-            res
-            .status(500)
-            .json(
-                {
-                    code: 'error',
-                    error: err.message,
-                }
-            );
-        });
-      })
-      .catch(err => {
-        res
-        .status (404)
-        .json ( {
-            code: 'error',
-            error: err.message,
-        })
-      });
+
+    const query = `SELECT `
 };
 export default login;
