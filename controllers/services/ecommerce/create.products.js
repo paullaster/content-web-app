b8411037c46aa60bf1a49f1ef8800ccccdfe0e58
paullaster <paullasterokoth98@gmail.com>
2022-12-21 19:10:39 +0300

    add files

--- controllers/services/ecommerce/create.products.js
+++ controllers/services/ecommerce/create.products.js
@@ -3,70 +3,44 @@ import db from "../../../utils/database.connection";
 import AttributeId from "../../../utils/create.attributeid";
 import productId from "../../../utils/create.productid";
 const createProduct = (req, res) => {
-    const { variation:{color, size, quantity}, ...product} = req.body;
+  const { variation: { color, size, quantity }, ...product } = req.body;
 
-    const productid = productId ();
-    let newProduct = {
-        ...product,
-        productid: productid,
-    };
-    let newAttribute = {
-        variation: AttributeId (),
-        color: color,
-        size: size, 
-        quantity: quantity,
-        productid: productid
-    };
-    const queryProd = `INSERT INTO products SET?`;
-    db.query(queryProd, newProduct, (err, rows) => {
-        if (err) {
-            res
-            .status(500)
-            .json( {
-                status: 'error',
-                error: err.message,
-            })
-            return;
-        };
-        
-    });
+  const productid = productId();
+  let newProduct = {
+    ...product,
+    productid: productid
+  };
+  let newAttribute = {
+    variation: AttributeId(),
+    color: color,
+    size: size,
+    quantity: quantity,
+    productid: productid
+  };
+  const queryProd = `INSERT INTO products SET?`;
+  db.query(queryProd, newProduct, (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        status: "error",
+        error: err.message
+      });
+      return;
+    }
 
-    
-    //saving product to db
-    newProduct.save ()
-    .then ((data) => {
-        newAttribute.save ()
-    .then ((data) => {
-        res
-        .status(200)
-        .json(
-            {
-                code: 'success',
-                message: 'product saved successfully'
-            }
-        );
-    })
-    .catch ((err) =>{
-        res
-        .status(500)
-        .json(
-            {
-                code: 'error',
-                error: err.message
-            }
-        )
+    const queryAttr = `INSERT INTO attributes SET?`;
+    db.query(queryAttr, newAttribute, (error, rows) => {
+      if (error) {
+        res.status(500).json({
+          status: "error",
+          error: error.message
+        });
+        return;
+      }
+      res.status(200).json({
+        status: "success",
+        message: "Product inserted successfully"
+      });
     });
-    })
-    .catch ((err) =>{
-        res
-        .status(500)
-        .json(
-            {
-                code: 'error',
-                error: err.message
-            }
-        )
-    });
-    
+  });
 };
 export default createProduct;
