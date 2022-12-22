21a2293f8500bb5b0ec4f561986dc2d1876fb64f
paullaster <paullasterokoth98@gmail.com>
2022-12-22 19:25:05 +0300

    add files

--- utils/verify.user.token.js
+++ utils/verify.user.token.js
@@ -3,11 +3,13 @@ import JWT from 'jsonwebtoken';
 const verifyToken = (req, res) => {
     const bearerToken = req.headers('Authorization');
     if (bearerToken === ''){
-        res.status().json( {
+        res.status(400).json( {
             status: 'error',
             error: 'Authorization required',
         })
         return;
-    }
+    };
+
+    const token = bearerToken.split(' ')[1];
 };
 export default verifyToken;
