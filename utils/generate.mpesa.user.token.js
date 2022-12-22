11095fd292758ea09c7031a0c25852f28242ee4e
paullaster <paullasterokoth98@gmail.com>
2022-12-22 21:59:37 +0300

    add files

--- utils/generate.mpesa.user.token.js
+++ utils/generate.mpesa.user.token.js
@@ -10,6 +10,7 @@ const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
 const generateToken = ( req, res, next) => {
     const apiUrl = `https://sandbox.safaricom.co.ke/oauth/v1/generate?
     grant_type=client_credentials`;
-     const token = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
+    const token = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
+    const auth = `Bearer ${token}`;
 };
 export default generateToken;
