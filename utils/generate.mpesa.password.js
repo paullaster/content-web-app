0aaa7cfecd4a5dcae7be80c1d1c30b6bfcb31cab
paullaster <paullasterokoth98@gmail.com>
2022-12-22 21:11:25 +0300

    add files

--- utils/generate.mpesa.password.js
+++ utils/generate.mpesa.password.js
@@ -5,3 +5,5 @@ import * as dotenv from 'dotenv';
 //Intantiating environment variables
 dotenv.config ();
 
+const passKey = process.env.MPESA_PASSKEY;
+const shortCode = process.env.MPESA_SHORT_CODE;
