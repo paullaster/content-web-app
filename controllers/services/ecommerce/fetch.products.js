7dcc2241d0718af161fcc3b065799e65c0469d99
paullaster <paullasterokoth98@gmail.com>
2022-12-22 16:59:25 +0300

    add files

--- controllers/services/ecommerce/fetch.products.js
+++ controllers/services/ecommerce/fetch.products.js
@@ -4,10 +4,12 @@ import db from "../../../utils/database.connection";
 //Find all products
 const fetchProducts = (req, res) =>{
     let query = `SELECT products.productid AS id , products.title AS title, 
-    products.description AS description, products.price AS price, images.image AS image,
-    attributes.variationid AS variation_id, attributes.color AS color, 
-    attributes.size AS size, attributes.quantity AS quantity FROM products
-    JOIN attributes ON attributes.productid = products.productid`;
+    products.description AS description, products.price AS price, 
+    images.path AS image, attributes.variationid AS variation_id, 
+    attributes.color AS color, attributes.size AS size, attributes.quantity AS quantity 
+    FROM products 
+    JOIN attributes ON attributes.productid = products.productid
+    JOIN images ON images.product = products.productid`;
     db.query(query, (err, rows) =>{
         if(err) {
             res.status(500).json ({
