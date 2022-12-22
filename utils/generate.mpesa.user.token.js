7b157621047de3bff27b646d2b8fb932a17501c4
paullaster <paullasterokoth98@gmail.com>
2022-12-22 21:52:29 +0300

    add files

--- utils/generate.mpesa.user.token.js
+++ utils/generate.mpesa.user.token.js
@@ -1,3 +1,7 @@
 import fetch from 'node-fetch';
 import * as dotenv from 'dotenv';
 dotenv.config ();
+
+//VARIABLES:
+const passKey = process.env.MPESA_PASSKEY;
+const shortCode = process.env.MPESA_SHORT_CODE;
