a15211c8ee82bca2ae8acf701901479a38ba82e7
paullaster <paullasterokoth98@gmail.com>
2022-12-22 19:22:52 +0300

    add files

--- utils/verify.user.token.js
+++ utils/verify.user.token.js
@@ -2,5 +2,11 @@ import JWT from 'jsonwebtoken';
 
 const verifyToken = (req, res) => {
     const bearerToken = req.headers('Authorization');
+    if (bearerToken === ''){
+        res.status().json( {
+            status: 'error',
+            error: 'Authorization required',
+        })
+    }
 };
 export default verifyToken;
