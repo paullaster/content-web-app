22d0505628a42d4af72676a1f510f84e3d254395
paullaster <paullasterokoth98@gmail.com>
2022-12-23 05:30:02 +0300

    add changes to a file

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -1,23 +1,33 @@
-import * as dotenv from 'dotenv';
-dotenv.config ();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 //Internal dependencies:
 import timeStamp from "../../../utils/timestamp";
-import newPassword from '../../../utils/generate.mpesa.password';
+import newPassword from "../../../utils/generate.mpesa.password";
 
 //LIPA NA MPESA API:
-const lipaNaMpesaOnline = ( req, res) => {
-    const token = req.accessToken;
-    const auth = 'Basic ' + token;
-    const timestamp = timeStamp ();
-    const apiUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
-    const businessShortCode = process.env.MPESA_BUSINESS_SHORT_CODE;
-    const password = newPassword ();
-    
-    //SEND REQUEST TO API:
-    fetch (apiUrl, {
-        
-    })
-    res.json ({token, auth, timestamp, businessShortCode, password});
+const lipaNaMpesaOnline = (req, res) => {
+  const token = req.accessToken;
+  const auth = "Basic " + token;
+  const timestamp = timeStamp();
+  const apiUrl =
+    "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
+  const password = newPassword();
+
+  //SEND REQUEST TO API:
+  fetch(apiUrl, {
+    BusinessShortCode: process.env.MPESA_BUSINESS_SHORT_CODE,
+    Password:
+    Timestamp: "20160216165627",
+    TransactionType: "CustomerPayBillOnline",
+    Amount: "1",
+    PartyA: "254708374149",
+    PartyB: "174379",
+    PhoneNumber: "254708374149",
+    CallBackURL: "https://mydomain.com/pat",
+    AccountReference: "Test",
+    TransactionDesc: "Test"
+  });
+  res.json({ token, auth, timestamp, businessShortCode, password });
 };
 export default lipaNaMpesaOnline;
