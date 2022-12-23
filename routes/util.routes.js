b8226a1ef0c20b4e37ca07ffe7586fbac7c9b2a6
paullaster <paullasterokoth98@gmail.com>
2022-12-23 06:14:36 +0300

    add changes to a file

--- routes/util.routes.js
+++ routes/util.routes.js
@@ -1,6 +1,8 @@
 import uploadProductImage from "../utils/upload.image";
 import verifyToken from "../utils/verify.user.token";
+import generateToken from "../utils/generate.mpesa.user.token";
 const utils = {
-    uploadProductImage, 
+    uploadProductImage,
+    verifyToken, 
 };
 export default utils;
