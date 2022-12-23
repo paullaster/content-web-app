8ac0aacfe0311f2830e3be7169eadf6aa76bcbc9
paullaster <paullasterokoth98@gmail.com>
2022-12-23 06:46:44 +0300

    add changes to a file

--- controllers/services/payment_api/payment.api.callback.js
+++ controllers/services/payment_api/payment.api.callback.js
@@ -1,2 +1,4 @@
-const paymentApiCallBack = ( req, res) => {};
+const paymentApiCallBack = ( req, res) => {
+    const callBackResultDescription = req.body.Body.stkCallback.;
+};
 export default paymentApiCallBack;
