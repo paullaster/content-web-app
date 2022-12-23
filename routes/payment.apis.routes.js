e1b07b0250ca6439126cdd23257a303a29befcc0
paullaster <paullasterokoth98@gmail.com>
2022-12-23 06:12:10 +0300

    add changes to a file

--- routes/payment.apis.routes.js
+++ routes/payment.apis.routes.js
@@ -2,3 +2,5 @@ import express from 'express';
 import PaymentAPI from "../controllers/services/payment_api";
 
 const PaymentAPIRouter = express.Router();
+
+PaymentAPIRouter.route('/stkpush').post()
