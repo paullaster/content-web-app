f0c0aeb9ed06ae2d42d44dfdfc5bceeb3ac13279
paullaster <paullasterokoth98@gmail.com>
2022-12-23 05:46:55 +0300

    add changes to a file

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -7,26 +7,27 @@ import newPassword from "../../../utils/generate.mpesa.password";
 
 //LIPA NA MPESA API:
 const lipaNaMpesaOnline = (req, res) => {
-    const phone = req.body.phone;
+    const phone = req.body.phone.substring(1);
+    const amount = req.body.amount;
   const token = req.accessToken;
   const auth = "Basic " + token;
   const apiUrl =
     "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
 
   //SEND REQUEST TO API:
-  fetch(apiUrl, {
-    BusinessShortCode: process.env.MPESA_BUSINESS_SHORT_CODE,
-    Password: newPassword(),
-    Timestamp: timeStamp(),
-    TransactionType: "CustomerBuyGoodsOnline",
-    Amount: "1",
-    PartyA: "254700258098",
-    PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
-    PhoneNumber: "254700258098",
-    CallBackURL: "https://mydomain.com/pat",
-    AccountReference: "Test",
-    TransactionDesc: "Test"
-  });
-  res.json({ token, auth, timestamp, businessShortCode, password });
+//   fetch(apiUrl, {
+//     BusinessShortCode: process.env.MPESA_BUSINESS_SHORT_CODE,
+//     Password: newPassword(),
+//     Timestamp: timeStamp(),
+//     TransactionType: "CustomerBuyGoodsOnline",
+//     Amount: "1",
+//     PartyA: "254700258098",
+//     PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
+//     PhoneNumber: "254700258098",
+//     CallBackURL: "https://mydomain.com/pat",
+//     AccountReference: "Test",
+//     TransactionDesc: "Test"
+//   });
+  res.json({ phone, amount });
 };
 export default lipaNaMpesaOnline;
