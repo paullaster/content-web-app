13cd94cebcee53c7198e5d31f64c5e50a2ef3bc0
paullaster <paullasterokoth98@gmail.com>
2022-12-23 05:23:17 +0300

    add changes to a file

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -1,13 +1,16 @@
 import * as dotenv from 'dotenv';
 dotenv.config ();
 
+//Internal dependencies:
 import timeStamp from "../../../utils/timestamp";
+import 
 const lipaNaMpesaOnline = ( req, res) => {
     const token = req.accessToken;
     const auth = 'Basic ' + token;
     const timestamp = timeStamp ();
     const apiUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
     const businessShortCode = process.env.MPESA_BUSINESS_SHORT_CODE;
+
     res.json ({token, auth, timestamp});
 };
 export default lipaNaMpesaOnline;
