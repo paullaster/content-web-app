7353cf4cc526cd5161621d7cf9037c8b9d07f742
paullaster <paullasterokoth98@gmail.com>
2022-12-23 06:20:36 +0300

    add changes to a file

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ import express from 'express';
 import blogs from './blog.routes';
 import auth from './auth.routes';
 import products from  './products.routes';
-import PaymentAPI from '../controllers/services/payment_api';
+import PaymentAPIRouter from './payment.apis.routes';
 
 const router = express.Router();
 
