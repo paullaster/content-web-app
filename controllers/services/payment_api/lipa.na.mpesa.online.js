eafabf0de9b4a359e0c9a221e03264d424a22240
paullaster <paullasterokoth98@gmail.com>
2022-12-23 05:19:16 +0300

    add files

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -3,6 +3,7 @@ const lipaNaMpesaOnline = ( req, res) => {
     const token = req.accessToken;
     const auth = 'Basic ' + token;
     const timestamp = timeStamp ();
+    const apiUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
     res.json ({token, auth, timestamp});
 };
 export default lipaNaMpesaOnline;
