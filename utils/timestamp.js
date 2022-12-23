d0919a99de15b939e8bd25a2d163a2496edf3653
paullaster <paullasterokoth98@gmail.com>
2022-12-23 05:07:13 +0300

    add files

--- utils/timestamp.js
+++ utils/timestamp.js
@@ -1,7 +1,7 @@
 const timestamp = () => {
     const date = new Date ();
     const year = date.getFullYear();
-    const month = ("0" + (date.getMonth() + 1));
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
     console.log (year, month);
 };
 timestamp ()
