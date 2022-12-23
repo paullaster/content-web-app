6a5b060e97b56d6c10ce8c26e7d1da0dcece1146
paullaster <paullasterokoth98@gmail.com>
2022-12-23 06:37:10 +0300

    add changes to a file

--- routes/payment.apis.routes.js
+++ routes/payment.apis.routes.js
@@ -5,6 +5,7 @@ import PaymentAPI from "../controllers/services/payment_api";
 import utils from './util.routes';
 
 const PaymentAPIRouter = express.Router();
+dotenv.config ();
 
 PaymentAPIRouter.route('/stkpush').post(utils.verifyToken, utils.generateToken, PaymentAPI.stkPush);
 PaymentAPIRouter.route()
