5d631d15585b8ffc012ef8fb0ade31e0b3698967
paullaster <paullasterokoth98@gmail.com>
2022-12-23 06:35:39 +0300

    add changes to a file

--- controllers/services/payment_api/index.js
+++ controllers/services/payment_api/index.js
@@ -3,5 +3,6 @@ import lipaNaMpesaOnline from "./lipa.na.mpesa.online"
 import paymentApiCallBack from "./payment.api.callback";
 const PaymentAPI = {
     stkPush: lipaNaMpesaOnline,
+    callback: paymentApiCallBack,
 };
 export default PaymentAPI;
