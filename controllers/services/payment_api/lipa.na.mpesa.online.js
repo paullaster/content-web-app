d0d6df8c3bfbcdef23a39278ec5e0a82cdb62a4d
paullaster <paullasterokoth98@gmail.com>
2022-12-23 05:20:12 +0300

    add files

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -1,9 +1,12 @@
+import * as dotenv from 'dotenv';
+
 import timeStamp from "../../../utils/timestamp";
 const lipaNaMpesaOnline = ( req, res) => {
     const token = req.accessToken;
     const auth = 'Basic ' + token;
     const timestamp = timeStamp ();
     const apiUrl = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+    const businessShortCode = 
     res.json ({token, auth, timestamp});
 };
 export default lipaNaMpesaOnline;
