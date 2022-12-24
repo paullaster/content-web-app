8ad941d408f662b58ae1892f361c80cac2afc9a3
paullaster <paullasterokoth98@gmail.com>
2022-12-24 10:50:49 +0300

    add changes to a file

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -6,9 +6,9 @@ import Product from '../controllers/services/ecommerce/index';
 import utils from './util.routes';
 const ProductRoutes = express.Router();
 
-ProductRoutes.route( '/new').post ( utils.uploadProductImage, Product.new);
+ProductRoutes.route( '/new').post ( utils.verifyToken, utils.uploadProductImage, Product.new);
 ProductRoutes.route( '/all_products').get (Product.all_products);
 ProductRoutes.route( '/one_products/:_id').get (Product.single_product);
-ProductRoutes.route( '/delete/:_id').delete (Product.delete);
-ProductRoutes.route( '/update/:_id').put (Product.update);
+ProductRoutes.route( '/delete/:_id').delete (utils.verifyToken, Product.delete);
+ProductRoutes.route( '/update/:_id').put (utils.verifyToken, utils.uploadProductImage, Product.update);
 export default ProductRoutes;
