8eafe5b1a098548af992ae3bb67b0da7da8fff30
paullaster <paullasterokoth98@gmail.com>
2022-12-28 17:20:22 +0300

    adding changes

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,9 +1,9 @@
 //Dependencies
-import express from 'express';
+const express = require ('express');
 
 //Internal dependencies
-import Product from '../controllers/services/ecommerce/index';
-import utils from './util.routes';
+const Product = require ('../controllers/services/ecommerce/index');
+const utils = require ('./util.routes');
 const ProductRoutes = express.Router();
 
 ProductRoutes.route( '/new').post ( utils.verifyToken, utils.uploadProductImage, Product.new);
@@ -11,4 +11,4 @@ ProductRoutes.route( '/all_products').get (Product.all_products);
 ProductRoutes.route( '/one_products/:_id').get (Product.single_product);
 ProductRoutes.route( '/delete/:_id').delete (utils.verifyToken, Product.delete);
 ProductRoutes.route( '/update/:_id').put (utils.verifyToken, utils.uploadProductImage, Product.update);
-export default ProductRoutes;
+module.exports = ProductRoutes;
