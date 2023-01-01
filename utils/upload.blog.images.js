c1a4133fa6a743f7726122a96f4bf8380c16777d
paullaster <paullasterokoth98@gmail.com>
2023-01-01 13:00:14 +0300

    add changes

---
+++ utils/upload.blog.images.js
@@ -0,0 +1,34 @@
+const multer = require ("multer");
+
+//Destination
+const multerStorage = multer.diskStorage( {
+    destination: (req, file, cb) => {
+        cb (null, 'public/images/products');
+    },
+    filename: (req, file, cb) => {
+        const extention = file.mimetype.split('/')[1];
+        cb(null, `${file.fieldname}_${Date.now()}.${extention}`);
+    },
+});
+
+//Filterin file type:
+const filterExtention = (req, file, cb) => {
+    if (file.mimetype.split('/')[1] === 'jpeg' ||
+        file.mimetype.split('/')[1] === 'jpg'  ||
+        file.mimetype.split('/')[1] === 'png'  ||
+        file.mimetype.split('/')[1] === 'svg'  ||
+        file.mimetype.split('/')[1] === 'gif'  ) {
+            cb(null, true);
+            return;
+    }
+    cb( new Error ( "File type not supported"), false);
+};
+
+// Intiatin multer
+const upload = multer ( {
+    storage: multerStorage,
+    fileFilter: filterExtention,
+});
+
+const uploadProductImage = upload.single('product_image')
+module.exports = uploadProductImage;
