fdab4d3adf8a6f13284cead6e7968992b6d05e78
paullaster <paullasterokoth98@gmail.com>
2023-01-01 13:11:48 +0300

    add changes

--- routes/util.routes.js
+++ routes/util.routes.js
@@ -1,4 +1,5 @@
 const uploadProductImage = require ("../utils/upload.image");
+const uploadBlogImage = require ( "../utils/upload.blog.images");
 const verifyToken = require ("../utils/verify.user.token");
 const generateToken = require ("../utils/generate.mpesa.user.token");
 const utils = {
