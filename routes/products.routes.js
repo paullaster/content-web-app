90ac465b4638a6dba27e3fddff6f46e1f02c7ffe
paullaster <paullasterokoth98@gmail.com>
2023-01-14 17:17:05 +0300

    add changes

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -6,7 +6,7 @@ const Product = require ('../controllers/services/ecommerce/index');
 const utils = require ('./util.routes');
 const ProductRoutes = express.Router();
 
-ProductRoutes.route( '/new').post ( utils.verifyToken, utils.uploadProductImage, Product.new);
+ProductRoutes.route( '/new').post ( utils.verifyToken, Product.new);
 ProductRoutes.route( '/all_products').get (Product.all_products);
 ProductRoutes.route( '/one_products/:productid').get (Product.single_product);
 ProductRoutes.route( '/delete/:productid').delete (utils.verifyToken, Product.delete);
