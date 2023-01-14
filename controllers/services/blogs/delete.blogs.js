a3cd45f60604f4651d39e66781bf31b9c35ad70b
paullaster <paullasterokoth98@gmail.com>
2023-01-14 13:19:56 +0300

    add changes

--- controllers/services/blogs/delete.blogs.js
+++ controllers/services/blogs/delete.blogs.js
@@ -3,7 +3,8 @@ const  db = require ('../../../utils/database.connection');
 const deleteBlogs = (req, res, next) => {
     const {blogid} = req.params;
     let query = `DELETE FROM blog WHERE blogid = '${blogid}'`;
-    db.query (query, (err, rows) => {
+    db.query (query)
+    .then((rows) => {
         if (err) {
             res
             .status (500)
