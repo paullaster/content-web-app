73b8d8711705ab6c7e834b39b6ea3eabd1e0896c
paullaster <paullasterokoth98@gmail.com>
2023-01-14 10:28:34 +0300

    add changes

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -26,51 +26,51 @@ const login = (req, res) => {
         });
         return;
       };
-      bcrypt
-        .compare(password, rows[0].password)
-        .then(resp => {
-          if (!resp) {
-            res.status(404).json({
-              status: "error",
-              error: "Invalid password"
-            });
-            return;
-          }
-          let payload = { email: rows[0].email };
-          JWT.sign(
-            payload,
-            process.env.TOKEN_SECRET,
-            {
-              algorithm: "HS512",
-              expiresIn: 5400
-            },
-            (err, token) => {
-              if (err) {
-                res.status(500).json({
-                  code: "error",
-                  error: "Error creating token"
-                });
-                return;
-              }
+    //   bcrypt
+    //     .compare(password, rows[0].password)
+    //     .then(resp => {
+    //       if (!resp) {
+    //         res.status(404).json({
+    //           status: "error",
+    //           error: "Invalid password"
+    //         });
+    //         return;
+    //       }
+    //       let payload = { email: rows[0].email };
+    //       JWT.sign(
+    //         payload,
+    //         process.env.TOKEN_SECRET,
+    //         {
+    //           algorithm: "HS512",
+    //           expiresIn: 5400
+    //         },
+    //         (err, token) => {
+    //           if (err) {
+    //             res.status(500).json({
+    //               code: "error",
+    //               error: "Error creating token"
+    //             });
+    //             return;
+    //           }
 
-              res
-                .status(200)
-                .header({
-                  Authorization: "Bearer " + token
-                })
-                .json({
-                  code: "success",
-                  token: token
-                });
-            }
-          );
-        })
-        .catch(err => {
-          res.status(500).json({
-            code: "error",
-            error: err.message
-          });
-        });
+    //           res
+    //             .status(200)
+    //             .header({
+    //               Authorization: "Bearer " + token
+    //             })
+    //             .json({
+    //               code: "success",
+    //               token: token
+    //             });
+    //         }
+    //       );
+    //     })
+    //     .catch(err => {
+    //       res.status(500).json({
+    //         code: "error",
+    //         error: err.message
+    //       });
+    //     });
     })
     .catch(err => {
       res.status(500).json({
