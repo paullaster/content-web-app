ea81a70d1e6980ed52199a4962576a4ad6415b7e
paullaster <paullasterokoth98@gmail.com>
2023-01-14 13:49:09 +0300

    add changes

--- controllers/services/blogs/fetch.blog.js
+++ controllers/services/blogs/fetch.blog.js
@@ -1,34 +1,27 @@
-const db = require ("../../../utils/database.connection");
+const db = require("../../../utils/database.connection");
 const fetchBlogs = (req, res) => {
-    let query = 'SELECT * FROM blog';
-    db.query(query)
-    .then((rows) => {
-        if (err) {
-            res
-            .status (500)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
-            return;
-        };
-        res
-        .status (200)
-        .json (
-            {
-                status: 'success',
-                message: 'Blogs retrieved successfully ',
-                data: rows,
-            }
-        );
+  let query = "SELECT * FROM blog";
+  db
+    .query(query)
+    .then(rows => {
+      if (err) {
+        res.status(500).json({
+          status: "error",
+          error: err.message
+        });
+        return;
+      }
+      res.status(200).json({
+        status: "success",
+        message: "Blogs retrieved successfully ",
+        data: rows
+      });
     })
-    .catch( (err) =>{
-        res
-            .status (500)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
-    }); 
+    .catch(err => {
+      res.status(500).json({
+        status: "error",
+        error: err.message
+      });
+    });
 };
 module.exports = fetchBlogs;
