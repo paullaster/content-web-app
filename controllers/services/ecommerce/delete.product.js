0b5b1be68d6668dbbe6d666b6c61197effdf06f3
paullaster <paullasterokoth98@gmail.com>
2023-01-14 14:41:13 +0300

    add changes

--- controllers/services/ecommerce/delete.product.js
+++ controllers/services/ecommerce/delete.product.js
@@ -4,7 +4,7 @@ const deleteProduct = (req, res) => {
   const { productid } = req.params;
 
   let query = `DELETE FROM products WHERE productid = '${productid}'`;
-  db.query(query, (err, rows) => {
+  db.query(query), (err, rows) => {
     if (err) {
       res.status(500).json({
         status: "error",
