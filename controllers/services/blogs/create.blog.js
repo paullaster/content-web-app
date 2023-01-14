d7f3ea714ed8b58813b3a8f018441155814b4ae7
paullaster <paullasterokoth98@gmail.com>
2023-01-14 13:09:09 +0300

    add changes

--- controllers/services/blogs/create.blog.js
+++ controllers/services/blogs/create.blog.js
@@ -1,34 +1,29 @@
-const  db = require ( '../../../utils/database.connection');
-const  blogId = require ( "../../../utils/create.blogid");
+const db = require("../../../utils/database.connection");
+const blogId = require("../../../utils/create.blogid");
 const createBlog = (req, res) => {
-    const {title, body} = req.body;
-    const blogid = blogId();
-    let newBlog = {
-        blogid,
-        title: title,
-        content: body,
-        image: req.file.path,
-        date: new Date(),
-    };
-    let query = 'INSERT INTO blog SET?';
-    db.query(query, newBlog, (err, rows) => {
-        if (err) {
-            res
-            .status (500)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
-            return;
-        };
-        res
-        .status (200)
-        .json(
-            {
-                status: 'success',
-                massage: blogid + ' created successfully'
-            }
-        )
+  const { title, body } = req.body;
+  const blogid = blogId();
+  let newBlog = {
+    blogid,
+    title: title,
+    content: body,
+    image: req.file.path,
+    date: new Date()
+  };
+  let query = "INSERT INTO blog SET?";
+  db.query(query, newBlog)
+  .then((err, rows) => {
+    if (err) {
+      res.status(500).json({
+        status: "error",
+        error: err.message
+      });
+      return;
+    }
+    res.status(200).json({
+      status: "success",
+      massage: blogid + " created successfully"
     });
+  });
 };
 module.exports = createBlog;
