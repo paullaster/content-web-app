29bf6bd27c8b9e436e3319d4df21a5ef561f7107
paullaster <paullasterokoth98@gmail.com>
2023-01-14 13:38:24 +0300

    add changes

--- controllers/services/blogs/delete.blogs.js
+++ controllers/services/blogs/delete.blogs.js
@@ -1,35 +1,28 @@
-const  db = require ('../../../utils/database.connection');
+const db = require("../../../utils/database.connection");
 
 const deleteBlogs = (req, res, next) => {
-    const {blogid} = req.params;
-    let query = `DELETE FROM blog WHERE blogid = '${blogid}'`;
-    db.query (query)
-    .then((rows) => {
-        if(rows[0].affectedRows < 1 ){
-            res
-            .status (400)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
-            return;
-        };
-        res
-        .status (200)
-        .json (
-            {
-                status: 'success',
-                message: 'Blog with id ' + blogid +' deleted',
-            }
-        );
+  const { blogid } = req.params;
+  let query = `DELETE FROM blog WHERE blogid = '${blogid}'`;
+  db
+    .query(query)
+    .then(rows => {
+      if (rows[0].affectedRows < 1) {
+        res.status(400).json({
+          status: "error",
+          error: err.message
+        });
+        return;
+      }
+      res.status(200).json({
+        status: "success",
+        message: "Blog with id " + blogid + " deleted"
+      });
     })
-    .catch ( (err) =>{
-            res
-            .status (500)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
+    .catch(err => {
+      res.status(500).json({
+        status: "error",
+        error: err.message
+      });
     });
 };
-module.exports= deleteBlogs
+module.exports = deleteBlogs;
