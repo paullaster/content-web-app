fb2c8dfde251eb12cff23bc74877f1e80b92bdf1
paullaster <paullasterokoth98@gmail.com>
2023-01-14 15:13:38 +0300

    add changes

--- controllers/services/ecommerce/update.product.js
+++ controllers/services/ecommerce/update.product.js
@@ -1,79 +1,76 @@
 //FETCH ALL PRODUCTS
-const db = require ("../../../utils/database.connection");
+const db = require("../../../utils/database.connection");
 
 const updateProduct = (req, res) => {
   const { productid } = req.params;
-  const {
-    title,
-    description,
-    price,
-    color, 
-    size, 
-    quantity 
-  } = req.body;
+  const { title, description, price, color, size, quantity } = req.body;
 
-  const {filename, path} = req.file;
+  const { filename, path } = req.file;
 
   let query = `UPDATE products SET title = '${title}', 
     description = '${description}',
     price = '${price}' WHERE productid = '${productid}'`;
 
-  db.query(query)
-  .then((rows) => {
-    if (rows[0].changedRows < 1) {
-      res.status(404).json({
-        status: "error",
-        error: "There was an error updating product",
-      });
-      return;
-    }
-    let query = `UPDATE attributes SET color = '${color}', 
-    size = '${size}', quantity = '${quantity}'
-    WHERE productid = '${productid}'`;
-    db.query(query)
-    .then((rows) => {
+  db
+    .query(query)
+    .then(rows => {
       if (rows[0].changedRows < 1) {
         res.status(404).json({
           status: "error",
-          error: "There was an error updating the product",
+          error: "There was an error updating product"
         });
         return;
       }
-      const sql = `UPDATE images SET imageid = '${filename}' 
-      path = '${path}', product = '${productid}' WHERE product = '${productid}'`;
-      db.query(sql)
-      .then((rows) => {
-        if (rows[0].changedRows < 1) {
-            res.status(400).json({
-                status: "error",
-                error: "There was an error updating the product",
+      let query = `UPDATE attributes SET color = '${color}', 
+    size = '${size}', quantity = '${quantity}'
+    WHERE productid = '${productid}'`;
+      db
+        .query(query)
+        .then(rows => {
+          if (rows[0].changedRows < 1) {
+            res.status(404).json({
+              status: "error",
+              error: "There was an error updating the product"
             });
             return;
-        };
-        res.status(200).json({
-            status: "success",
-            message: "Product  with id " +productid + " updated successfully"
+          }
+          const sql = `UPDATE images SET imageid = '${filename}' 
+      path = '${path}', product = '${productid}' WHERE product = '${productid}'`;
+          db
+            .query(sql)
+            .then(rows => {
+              if (rows[0].changedRows < 1) {
+                res.status(400).json({
+                  status: "error",
+                  error: "There was an error updating the product"
+                });
+                return;
+              }
+              res.status(200).json({
+                status: "success",
+                message:
+                  "Product  with id " + productid + " updated successfully"
+              });
+            })
+            .catch(err => {
+              res.status(400).json({
+                status: "error",
+                error: err.message
+              });
+            });
+        })
+        .catch(err => {
+          res.status(500).json({
+            status: "error",
+            error: err.message
+          });
         });
-      })
-      .catch ( (err) => {
-        res.status(400).json({
-          status: "error",
-          error: err.message
-      });
-      });
     })
-    .catch ( (err) => {
+    .catch(err => {
       res.status(500).json({
         status: "error",
         error: err.message
+      });
     });
-  });
-  })
-  .catch ( (err) => {
-    res.status(500).json({
-      status: "error",
-      error: err.message
-  });
-  });
 };
 module.exports = updateProduct;
