29d26e2f89860e9e24cfb3de5b90790890460840
paullaster <paullasterokoth98@gmail.com>
2023-01-14 13:21:06 +0300

    add changes

--- controllers/services/blogs/delete.blogs.js
+++ controllers/services/blogs/delete.blogs.js
@@ -5,15 +5,6 @@ const deleteBlogs = (req, res, next) => {
     let query = `DELETE FROM blog WHERE blogid = '${blogid}'`;
     db.query (query)
     .then((rows) => {
-        if (err) {
-            res
-            .status (500)
-            .json ( {
-                status: 'error',
-                error: err.message,
-            });
-            return;
-        };
         res
         .status (200)
         .json (
@@ -24,7 +15,7 @@ const deleteBlogs = (req, res, next) => {
         );
     })
     .catch ( (err) =>{
-        
+
     });
 };
 module.exports= deleteBlogs
