a40982418c5ddaf564af0561d7f883d82a350a35
paullaster <paullasterokoth98@gmail.com>
2023-01-14 17:11:09 +0300

    add changes

--- controllers/services/ecommerce/create.products.js
+++ controllers/services/ecommerce/create.products.js
@@ -69,29 +69,6 @@ const createProduct = (req, res) => {
             });
 
           });
-          const sql = `INSERT INTO images SET?`;
-          db
-            .query(sql, newImage)
-            .then(rows => {
-              if (rows[0].affectedRows < 1) {
-                res.status(500).json({
-                  status: "error",
-                  error: "there was an error processing the product"
-                });
-                return;
-              }
-              res.status(200).json({
-                status: "success",
-                message:
-                  "Product  with id '" + productid + "' inserted successfully"
-              });
-            })
-            .catch(err => {
-              res.status(500).json({
-                status: "error",
-                error: err.message
-              });
-            });
         })
         .catch(error => {
           res.status(500).json({
