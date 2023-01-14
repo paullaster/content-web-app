06f530b022b18dd11dc2d2dd355c3123287e1444
paullaster <paullasterokoth98@gmail.com>
2023-01-14 16:56:18 +0300

    add changes

--- controllers/services/ecommerce/create.products.js
+++ controllers/services/ecommerce/create.products.js
@@ -2,6 +2,7 @@
 const db = require("../../../utils/database.connection");
 const AttributeId = require("../../../utils/create.attributeid");
 const productId = require("../../../utils/create.productid");
+const uploadProductImage = require ( './utils/upload.image');
 
 const createProduct = (req, res) => {
   const { color, size, quantity, ...product } = req.body;
