deceadd5775c424346c848ba1eac051aab5cd1c9
paullaster <paullasterokoth98@gmail.com>
2023-01-14 10:30:03 +0300

    add changes

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -26,7 +26,7 @@ const login = (req, res) => {
         });
         return;
       };
-      res.json({password, hash:rows[0].password});
+      res.json({password, hash:'rows[0].password}');
     //   bcrypt
     //     .compare(password, rows[0].password)
     //     .then(resp => {
