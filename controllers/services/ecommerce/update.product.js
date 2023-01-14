f74c34f53a76d57f47cf12faf919d96cd32767ac
paullaster <paullasterokoth98@gmail.com>
2023-01-14 15:04:29 +0300

    add changes

--- controllers/services/ecommerce/update.product.js
+++ controllers/services/ecommerce/update.product.js
@@ -18,7 +18,7 @@ const updateProduct = (req, res) => {
     description = '${description}',
     price = '${price}' WHERE productid = '${productid}'`;
 
-  db.query(query, (err, result) => {
+  db.query(query).then((err, result) => {
     if (err) {
       res.status(404).json({
         status: "error",
