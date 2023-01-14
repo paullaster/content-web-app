09650ecaeadfe51b3b80cbed2dffb61a2488c4b1
paullaster <paullasterokoth98@gmail.com>
2023-01-14 15:48:09 +0300

    add changes

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ app.get ('/', (req, res) => {
     res.json('Welcome to goebaide services');
 });
 const uploadProductImage = require ( './utils/upload.image');
-app.post ('/test', (req, res) => {});
+app.post ('/test', uploadProductImage, (req, res) => {
+    console.log (req.file)
+    res.json('image');
+});
 //Instanciating server
 app.listen (PORT, () => {
     console.log (`App listening on port ${PORT}`);
