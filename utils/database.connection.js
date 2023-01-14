bf3352c94b26820019988ab59ce6840c1a21ef99
paullaster <paullasterokoth98@gmail.com>
2023-01-14 09:23:09 +0300

    add changes

--- utils/database.connection.js
+++ utils/database.connection.js
@@ -3,7 +3,7 @@ const dotenv = require ('dotenv');
 dotenv.config();
 
 //DB connection:
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
