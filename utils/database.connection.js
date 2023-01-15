3a5c9695e6c3ad2498bd9af5230a7892e4b843b2
paullaster <paullasterokoth98@gmail.com>
2023-01-15 05:52:16 +0300

    commit file changes

--- utils/database.connection.js
+++ utils/database.connection.js
@@ -10,7 +10,8 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit:0
+  queueLimit:0,
+  multipleStatements: true
 });
 
 const db = pool.promise();
