1c8808a14bd17e8032a4ee6296260ff68419c453
paullaster <paullasterokoth98@gmail.com>
2023-01-15 20:19:53 +0300

    commit files

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const blogs = require ('./blog.routes');
 const auth = require ('./auth.routes');
 const products = require ( './products.routes');
 const PaymentAPIRouter = require ('./payment.apis.routes');
+const orders = require ('./orders.routes');
 
 const router = express.Router();
 
