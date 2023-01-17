a074735ac0cea37bf7cacd3f0676a78a92f2037c
paullaster <paullasterokoth98@gmail.com>
2023-01-17 07:06:33 +0300

    add files changes

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -1,56 +1,60 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const dotenv = require ( "dotenv");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const dotenv = require("dotenv");
 dotenv.config();
 
 //Internal dependencies:
-const timeStamp = require ("../../../utils/timestamp");
-const newPassword = require ("../../../utils/generate.mpesa.password");
+const timeStamp = require("../../../utils/timestamp");
+const newPassword = require("../../../utils/generate.mpesa.password");
 
 //LIPA NA MPESA API:
 const lipaNaMpesaOnline = (req, res) => {
-    const phone = req.body.phone.substring(1);
-    const amount = req.body.amount;
+  const phone = req.body.phone.substring(1);
+  const amount = req.body.amount;
   const token = req.accessToken;
   const auth = "Bearer " + token;
   const apiUrl =
     "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest";
 
   //SEND REQUEST TO API:
-  fetch(apiUrl, {
-    BusinessShortCode: process.env.MPESA_BUSINESS_SHORT_CODE,
-    Password: newPassword(),
-    Timestamp: timeStamp(),
-    TransactionType: "CustomerBuyGoodsOnline",
-    Amount: amount,
-    PartyA: `254${phone}`,
-    PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
-    PhoneNumber: `254${phone}`,
-    CallBackURL: 
-    `https://0341-105-163-156-86.in.ngrok.io/api/payment/${process.env.MPESA_CALL_BACK_API_NAME}`,
-    AccountReference: `254${phone}`,
-    TransactionDesc: "Payment for product purchase"
-  },
-  {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: auth,
+  fetch(
+    apiUrl,
+    {
+      BusinessShortCode: process.env.MPESA_BUSINESS_SHORT_CODE,
+      Password: newPassword(),
+      Timestamp: timeStamp(),
+      TransactionType: "CustomerBuyGoodsOnline",
+      Amount: amount,
+      PartyA: `254${phone}`,
+      PartyB: process.env.MPESA_BUSINESS_SHORT_CODE,
+      PhoneNumber: `254${phone}`,
+      CallBackURL: `https://0341-105-163-156-86.in.ngrok.io/api/payment/${process
+        .env.MPESA_CALL_BACK_API_NAME}`,
+      AccountReference: `254${phone}`,
+      TransactionDesc: "Payment for product purchase"
     },
-  })
-  .then(response => response.json())
-  .then ( (response) => {
-    res.status(200).json ( {
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer SpCigG0UVmpinUgrpqrTXoX5hwuA"
+      }
+    }
+  )
+    .then(response => response.json())
+    .then(response => {
+      res.status(200).json({
         status: "success",
         Password: newPassword(),
-        token:auth,
+        token: auth,
         Timestamp: timeStamp(),
-        data: response,
-    });
-  })
-  .catch ( (err) => {
-    res.status(500).json({
+        data: response
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
         status: "error",
-        error: err.message,
+        error: err.message
+      });
     });
-  });
 };
 module.exports = lipaNaMpesaOnline;
