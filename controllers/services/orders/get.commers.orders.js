5dfd61afd37f41712abb5a0c476e0e8207d776e7
paullaster <paullasterokoth98@gmail.com>
2023-01-17 05:52:16 +0300

    add files changes

--- controllers/services/orders/get.commers.orders.js
+++ controllers/services/orders/get.commers.orders.js
@@ -1,5 +1,6 @@
 const customerOrders = (req, res, next) => {
     const [ order_details,  amount, address, paymentDetail] = req.body;
+    
     res.json(paymentDetail);
 };
 module.exports = customerOrders;
