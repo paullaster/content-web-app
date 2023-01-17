f20b59d30061578516776ed833f0447e4bd1dc88
paullaster <paullasterokoth98@gmail.com>
2023-01-17 05:54:29 +0300

    add files changes

--- controllers/services/orders/get.commers.orders.js
+++ controllers/services/orders/get.commers.orders.js
@@ -1,7 +1,10 @@
+//DEPENDENCIES:
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const customerOrders = (req, res, next) => {
     const [ order_details,  amount, address, paymentDetail] = req.body;
 
-    
+
     res.json(paymentDetail);
 };
 module.exports = customerOrders;
