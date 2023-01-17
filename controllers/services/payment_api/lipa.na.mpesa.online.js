f9d3b2c3ea009fa74e40ff656acb8559e9ec5d16
paullaster <paullasterokoth98@gmail.com>
2023-01-17 06:01:22 +0300

    add files changes

--- controllers/services/payment_api/lipa.na.mpesa.online.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.js
@@ -39,7 +39,7 @@ const lipaNaMpesaOnline = (req, res) => {
   .then ( (response) => {
     res.status(200).json ( {
         status: "success",
-        data: response.data,
+        data: response,
     });
   })
   .catch ( (err) => {
