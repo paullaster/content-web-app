9605bff611a13674e3270b19d2ed7f1df6dc78d5
paullaster <paullasterokoth98@gmail.com>
2023-01-18 18:40:36 +0300

    commit files

--- controllers/services/payment_api/index.js
+++ controllers/services/payment_api/index.js
@@ -1,8 +1,10 @@
 
 const lipaNaMpesaOnline = require ("./lipa.na.mpesa.online");
 const paymentApiCallBack = require ("./payment.api.callback");
+const queryLipaNaMpesaOnline = require ("./lipa.na.mpesa.online.query.api");
 const PaymentAPI = {
     stkPush: lipaNaMpesaOnline,
     callback: paymentApiCallBack,
+    
 };
 module.exports = PaymentAPI;
