8f2b14c63164245077cad09202e13b98a2b2f388
paullaster <paullasterokoth98@gmail.com>
2023-01-18 10:33:03 +0300

    commit files

--- routes/payment.apis.routes.js
+++ routes/payment.apis.routes.js
@@ -7,7 +7,7 @@ const utils = require ('./util.routes');
 const PaymentAPIRouter = express.Router();
 dotenv.config ();
 
-PaymentAPIRouter.route(`/${process.env.MPESA_CALL_BACK_API_NAME}`).post(utils.verifyToken, utils.generateToken, PaymentAPI.stkPush);
+PaymentAPIRouter.route(`/${process.env.MPESA_STK_PUSH_URI}`).post(utils.verifyToken, utils.generateToken, PaymentAPI.stkPush);
 PaymentAPIRouter.route(`/${process.env.MPESA_CALL_BACK_API_NAME}`)
 .post(utils.verifyToken, utils.generateToken, PaymentAPI.callback);
 module.exports = PaymentAPIRouter;
