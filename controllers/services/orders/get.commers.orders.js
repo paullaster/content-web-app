df749fa0cfeb8d88de4e65b3301800611bf72b4c
paullaster <paullasterokoth98@gmail.com>
2023-01-18 10:46:15 +0300

    commit files

--- controllers/services/orders/get.commers.orders.js
+++ controllers/services/orders/get.commers.orders.js
@@ -1,18 +1,18 @@
 //DEPENDENCIES:
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const dotenv = require('dotenv').config();
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const dotenv = require("dotenv").config();
 
 //INTERNAL DEPENDENCIES:
 
-
 const customerOrders = (req, res, next) => {
-    const [ order_details,  amount, address, paymentDetail] = req.body;
-    const paymentBody = {
-        phone: paymentDetail.paymentDetail,
-        amount: amount.amount
-    };
-    const PAYMENT_URI = `http://localhost:${process.env.APP_PORT}/payment/`
-    //res.json({paymentDetail,amount:});
-
+  const [order_details, amount, address, paymentDetail] = req.body;
+  const paymentBody = {
+    phone: paymentDetail.paymentDetail,
+    amount: amount.amount
+  };
+  const PAYMENT_URI = `http://localhost:${process.env
+    .APP_PORT}/payment/${process.env.MPESA_STK_PUSH_URI}`;
+  //res.json({paymentDetail,amount:});
 };
 module.exports = customerOrders;
