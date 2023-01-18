a017faf8cdf5e34d9d3a9e057ed287d39edfd61e
paullaster <paullasterokoth98@gmail.com>
2023-01-18 12:33:50 +0300

    commit files

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -3,7 +3,7 @@ const OrdersRoute = express.Router();
 
 //INTERNAL DEPENDECIES:
 const Orders = require ('../controllers/services/orders/index');
-const utils = 
+const utils = require('./util.routes');
 
 OrdersRoute.route('/new-order').post(Orders.newOrders);
 module.exports = OrdersRoute;
