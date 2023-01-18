2e8ca7dcb02f7c3d03e8340dce21bf0048c08ea2
paullaster <paullasterokoth98@gmail.com>
2023-01-18 23:10:24 +0300

    commit files

--- controllers/services/payment_api/lipa.na.mpesa.online.query.api.js
+++ controllers/services/payment_api/lipa.na.mpesa.online.query.api.js
@@ -2,6 +2,8 @@ const newPassword = require ("../../../utils/generate.mpesa.password");
 const timeStamp = require ("../../../utils/timestamp");
 const queryLipaNaMpesaOnline = (req, res) => {
 
+    const statusQueryURI = 
+    " https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query";
     console.log ("req.body");
 };
 module.exports = queryLipaNaMpesaOnline;
