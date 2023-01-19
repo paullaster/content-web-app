8a4632a062a0148b0aec79122a50ce0123088d88
paullaster <paullasterokoth98@gmail.com>
2023-01-19 13:26:52 +0300

    add file changes

--- controllers/services/ecommerce/fetch.products.js
+++ controllers/services/ecommerce/fetch.products.js
@@ -1,34 +1,35 @@
 //FETCH ALL PRODUCTS
-const db = require ("../../../utils/database.connection");
+const db = require("../../../utils/database.connection");
 
 //Find all products
-const fetchProducts = (req, res) =>{
-    let query = `SELECT products.productid AS id , products.title AS title, 
+const fetchProducts = (req, res) => {
+  let query = `SELECT products.productid AS id , products.title AS title, 
     products.description AS description, products.price AS price, 
     images.path AS image, attributes.variationid AS variation_id, 
     attributes.color AS color, attributes.size AS size, attributes.quantity AS quantity 
     FROM products 
     JOIN attributes ON attributes.productid = products.productid
     JOIN images ON images.product = products.productid`;
-    db.query(query)
-    .then((rows) =>{
-        if(rows[0].length < 1) {
-            res.status(400).json ({
-                status: 'error',
-                error: "Products are not available",
-            });
-            return;
-        };
-        res.status(200).json({
-            status: 'success',
-            data: rows[0],
+  db
+    .query(query)
+    .then(rows => {
+      if (rows[0].length < 1) {
+        res.status(400).json({
+          status: "error",
+          error: "Products are not available"
         });
+        return;
+      }
+      res.status(200).headers().json({
+        status: "success",
+        data: rows[0]
+      });
     })
-    .catch ( (err) => {
-        res.status(500).json ({
-            status: 'error',
-            error: err.message,
-        });
+    .catch(err => {
+      res.status(500).json({
+        status: "error",
+        error: err.message
+      });
     });
 };
-module.exports =  fetchProducts;
+module.exports = fetchProducts;
