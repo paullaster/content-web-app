97f9a9030c5bc0147a4fd134aea360c66cdefef0
paullaster <paullasterokoth98@gmail.com>
2023-01-19 05:43:18 +0300

    add file changes

--- utils/create.itemid.js
+++ utils/create.itemid.js
@@ -1,5 +1,5 @@
 const itemId = () => {
-    const statement = 'blog';
+    const statement = 'item';
     let randNumber = [];
     for ( let i = 0; randNumber.length < 4; i++ ) {
         let randDigit = Math.floor( Math.random() * 10);
