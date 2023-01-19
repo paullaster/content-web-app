d4ef76d63f658439633a664ae510a156efe3c92a
paullaster <paullasterokoth98@gmail.com>
2023-01-19 05:45:16 +0300

    add file changes

---
+++ utils/create.addressid.js
@@ -0,0 +1,11 @@
+const addressId = () => {
+    const statement = 'blog';
+    let randNumber = [];
+    for ( let i = 0; randNumber.length < 4; i++ ) {
+        let randDigit = Math.floor( Math.random() * 10);
+        randNumber = [...randNumber, randDigit];
+    };
+    
+    return statement + '' + randNumber.join('');
+};
+module.exports = addressId;
