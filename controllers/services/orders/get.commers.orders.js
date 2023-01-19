d4b8b1d25a78840370690d4267fa8b5394d6c53c
paullaster <paullasterokoth98@gmail.com>
2023-01-19 06:10:03 +0300

    add file changes

--- controllers/services/orders/get.commers.orders.js
+++ controllers/services/orders/get.commers.orders.js
@@ -23,6 +23,9 @@ const customerOrders = (req, res, next) => {
    * Take order item processing inside the block
    * of checking if transcation payment was done successfully
    */
+
+
+  
   //SENDING DATA TO MPESA EXPRESS API TO MAKE PAYMENTS:
   const paymentBody = JSON.stringify({
     phone: paymentDetail.paymentDetail,
