307ed1e28ddd6d4a02e945c5cae2c96677f864fc
paullaster <paullasterokoth98@gmail.com>
2023-01-19 06:43:47 +0300

    add file changes

--- controllers/services/orders/get.commers.orders.js
+++ controllers/services/orders/get.commers.orders.js
@@ -58,7 +58,7 @@ const customerOrders = (req, res, next) => {
         /**
    * @todo: Remove this implementation to successful transitions block:
    */
-        const sql = `INSERT INTO order_item (itemid, name, orderid, productid, image, size, quantity)`;
+        const sql = `INSERT INTO order_item (itemid, name, orderid, productid, image, size, quantity) VALUES?`;
         res.status(404).json({
           status: "error",
           error:
