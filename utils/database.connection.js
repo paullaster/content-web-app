ce83f06142f18e15c243c6a414d773afa7fe2102
paullaster <paullasterokoth98@gmail.com>
2023-01-25 18:14:35 +0300

    remove mpesa payment

--- utils/database.connection.js
+++ utils/database.connection.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password:"",
+  password:process.env.DB_USER,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
