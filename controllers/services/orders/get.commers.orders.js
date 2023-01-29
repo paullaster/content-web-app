70637ffe8ad744bb245b97c16fbed7d4da92d545
paullaster <paullasterokoth98@gmail.com>
2023-01-29 11:13:15 +0300

    add changes

--- controllers/services/orders/get.commers.orders.js
+++ controllers/services/orders/get.commers.orders.js
@@ -64,7 +64,7 @@ const customerOrders = (req, res, next) => {
    * of checking if transcation payment was done successfully
    * SAVING ORDER:
     */ 
-          //SAVING ORDER ITEM:
+                  //SAVING ORDER ITEM:
                   const sql = `INSERT INTO order_item (itemid, name, orderid, productid, image, size, quantity) VALUES?`;
                   db
                     .query(sql, [newOrderItems])
@@ -98,6 +98,38 @@ const customerOrders = (req, res, next) => {
                             });
                             return;
                           }
+                          const newOrder = {
+                            orderid: orderid,
+                            customer_id: user
+                          };
+                          const sql = `INSERT INTO orders SET?`;
+                          db
+                            .query(sql, newOrder)
+                            .then(rows => {
+                              if (rows[0].affectedRows < 1) {
+                                res.status(500).json({
+                                  status: "error",
+                                  error: "there was an error processing this order"
+                                });
+                                return;
+                              }
+                              const newOrderItems = order_details.order_details.map(
+                                item => {
+                                  return [
+                                    itemId(),
+                                    item.title,
+                                    orderid,
+                                    item.id,
+                                    item.image,
+                                    item.itemSize,
+                                    item.itemQuantityToBuy
+                                  ];
+                                }
+                              );
+            
+                              /**
+                   * @done: Remove this implementation to successful transitions block:
+                   */
                           res.status(200).json({
                             status: "success",
                             message:
